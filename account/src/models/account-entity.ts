c7bec5b81f2d317f947868ab1e6fc3d5d1eb298b
nour <nour.elamrichatbri@poe.aelion.fr>
2024-07-25 16:15:32 +0200

    Set up TypeORM connection to MariaDB and created AccountEntity

---
+++ account/src/models/account-entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum Role {
+  INTERN = 'intern',
+  ADMIN = 'admin',
+}
+@Entity({
+  name: 'account',
+})
+export class AccountEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.INTERN,
+  })
+  role: Role;
+
+  @Column()
+  isActivated: boolean;
+}
