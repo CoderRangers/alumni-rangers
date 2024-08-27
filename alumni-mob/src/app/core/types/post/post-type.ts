632ac31f4e9f9cea20758495966ae6b581e3604e
nour <nour.elamrichatbri@poe.aelion.fr>
2024-08-27 12:55:38 +0200

    Creation of post type and comment type

--- alumni-mob/src/app/core/types/post/post-type.ts
+++ alumni-mob/src/app/core/types/post/post-type.ts
@@ -1,10 +1,22 @@
 import { InternType } from "../intern/intern-type"
+import { CommentType } from "./comment-type"
 
+
+enum PostCategory {
+    "Offre d'emploi",
+    "Actualités",
+    "Offre de stage"
+
+}
 export type PostType = {
-    id?: number
+    id?: string
     title?: string
     content: string
     media?: string
     postedAt: Date
     author: InternType
+    likes?: Array<InternType>
+    comments?: Array<CommentType>
+    category: PostCategory
+
 }
