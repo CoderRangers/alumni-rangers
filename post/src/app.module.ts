2ff5ef5fafce57fca4b1d04b9303a760f518b49f
jean <jean.soulie@poe.aelion.fr>
2024-08-27 16:06:48 +0200

    connexion between microservice post and intern

--- post/src/app.module.ts
+++ post/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConnectModule } from './connect.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ConnectModule],
+  imports: [
+    ConnectModule,
+    ClientsModule.register([
+      {
+        name: 'INTERN',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3100,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- post/src/app.service.ts
+++ post/src/app.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PostEntity } from './models/post-entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(PostEntity)
     private _repository: Repository<PostEntity>,
+    @Inject('INTERN') private _client: ClientProxy,
   ) {}
 
   getAllPosts(): Promise<Array<PostEntity>> {
