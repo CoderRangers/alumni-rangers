1447a22579530c582cb59ffeedac84a1b6931846
Remi <remi.salvan@poe.aelion.fr>
2024-08-27 10:54:43 +0200

    cleaning dto files

--- gateway/src/account/account.controller.ts
+++ gateway/src/account/account.controller.ts
@@ -1,37 +1,37 @@
-import { Controller, Get, Logger, Param, Res } from '@nestjs/common'
-import { AccountService } from './account.service'
-import { Observable, take } from 'rxjs'
-import { AccountType } from './model/account-type'
-import { Response } from 'express'
+import { Controller, Get, Logger, Param, Res } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { Observable, take } from 'rxjs';
+import { AccountType } from './model/account-type';
+import { Response } from 'express';
 
 @Controller('account')
 export class AccountController {
-    constructor(private readonly accountService: AccountService) {}
+  constructor(private readonly accountService: AccountService) {}
 
-    @Get()
-    findAll(): Observable<Array<AccountType>> {
-        return this.accountService.findAll().pipe(take(1))
-    }
+  @Get()
+  findAll(): Observable<Array<AccountType>> {
+    return this.accountService.findAll().pipe(take(1));
+  }
 
-    @Get(':email')
-    findOne(@Param('email') email: string, @Res() res: Response) {
-        const typeEmail: string = email
-        Logger.log('typeEmail: ' + typeEmail)
-        return this.accountService
-            .findOne(typeEmail)
-            .pipe(take(1))
-            .subscribe({
-                next: (response: any) => {
-                    Logger.log('response: ' + response)
-                    if (response) {
-                        res.status(200).send(response)
-                    } else {
-                        res.status(404).send()
-                    }
-                },
-                error: (error: any) => {
-                    res.status(500).send(error)
-                },
-            })
-    }
+  @Get(':email')
+  findOne(@Param('email') email: string, @Res() res: Response) {
+    const typeEmail: string = email;
+    Logger.log('typeEmail: ' + typeEmail);
+    return this.accountService
+      .findOne(typeEmail)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          Logger.log('response: ' + response);
+          if (response) {
+            res.status(200).send(response);
+          } else {
+            res.status(404).send();
+          }
+        },
+        error: (error: any) => {
+          res.status(500).send(error);
+        },
+      });
+  }
 }
