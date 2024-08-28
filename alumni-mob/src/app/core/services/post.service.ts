fe67350132bc1dbafdedddb434fa01cca6d3c574
nour <nour.elamrichatbri@poe.aelion.fr>
2024-08-28 12:03:13 +0200

    adding some posts

--- alumni-mob/src/app/core/services/post.service.ts
+++ alumni-mob/src/app/core/services/post.service.ts
@@ -41,7 +41,7 @@ export class PostService {
           firstname: "Cyril",
           lastname: "Baille",
           gender: "M",
-          mails: ["mail3", "mail4"],
+          mails: ["mail1@aelion.com"],
           phone: "0605040311",
           occupation: "Scrum Master",
           company: {
@@ -66,11 +66,11 @@ export class PostService {
         postedAt: new Date('2024-08-26T13:32:03'),
         author: 
         {
-          id:2,
+          id: 3,
           firstname: "Antoine",
           lastname: "Dupont",
           gender: "M",
-          mails: ["mail1", "mail2"],
+          mails: ["mail6@aelion.com"],
           phone: "0605040311",
           occupation: "Développeur Fullstack",
           company: {
@@ -89,17 +89,46 @@ export class PostService {
       },
       {
         id: 'c',
-        title: 'Soutenance des stagiaires POEC dev mobile',
-        content: 'le développement d\'un reseau social privé pour le centre de formation AELION est enfin achevé par les efforts des stagiaires de centre et sous l\'encadrement de notre devéloppeur JEAN LUC AUBERT',
-        media: '/assets/icon/aelion_logo.png',
+        title: 'Début du POEC développemtn mobile',
+        content: 'Nous  lonçons notre promotion de Développeur d\'application mobile, une nouvelle aventure commence pour ces 15 alternants.',
+        media: '/assets/1719410226224.jpeg',
         postedAt: new Date('2024-08-25T22:42:00'),
         author: 
         {
           id:3,
+          firstname: "Ibrahim",
+          lastname: "Missaoui",
+          gender: "M",
+          mails: ["mail3@aelion.com"],
+          phone: "0605040347",
+          occupation: "Chargé de recrutement",
+          company: {
+              id: 1,
+              name: "Ludwig"
+              },
+          poe: {
+              id: 5,
+              beginAt: new Date("2024/05/05"),
+              endAt: new Date("2024/07/05"),
+              name: "IMMERSION PRO",
+              type: "INTÉGRATION"
+          }
+      },
+        category: PostCategory.news,
+      },
+      {
+        id: 'c',
+        title: 'Soutenance des stagiaires POEC dev mobile',
+        content: 'le développement d\'un reseau social privé pour le centre de formation AELION est enfin achevé par les efforts des stagiaires de centre et sous l\'encadrement de notre devéloppeur JEAN LUC AUBERT',
+        media: '/assets/icon/aelion_logo.png',
+        postedAt: new Date('2024-08-24T22:42:00'),
+        author: 
+        {
+          id:4,
           firstname: "Mathilde",
           lastname: "Ntamack",
-          gender: "M",
-          mails: ["mail5", "mail6"],
+          gender: "F",
+          mails: ["mail6@aelion.com"],
           phone: "0605040347",
           occupation: "Consultante fonctionnelle",
           company: {
@@ -112,6 +141,64 @@ export class PostService {
               endAt: new Date("2024/07/05"),
               name: "POEC DEVELOPPEMENT",
               type: "POEC"
+          }
+      },
+        category: PostCategory.news,
+      },
+      {
+        id: 'a',
+        title: 'Offre d\'emploi',
+        content: 'Développeur Fullstack chez Ludwig qui aura comme mission la conception et le développement de nouvelles solutions e-commerces pour le groupe',
+        media: '',
+        postedAt: new Date('2024-08-23T09:10:15'),
+        author: 
+        {
+          id:1,
+          firstname: "Cyril",
+          lastname: "Baille",
+          gender: "M",
+          mails: ["mail1@aelion.com"],
+          phone: "0605040311",
+          occupation: "Scrum Master",
+          company: {
+              id: 1,
+              name: "CAPGEMINI"
+              },
+          poe: {
+              id: 2,
+              beginAt: new Date("2024/05/05"),
+              endAt: new Date("2024/07/05"),
+              name: "Développement d'application mobile",
+              type: "POEI"
+          }
+      },
+        category: PostCategory.news,
+      },
+      {
+        id: 'k',
+        title: 'Vaccance',
+        content: 'Bonjour la tribu, c\'est enfin les vaccance, toute l\'équipe Aelion tient à vous souhaiter de trés belle vaccance! Profitez de chaque instant et revenez plein d\'énergie et de beaux souvenirs',
+        media: '',
+        postedAt: new Date('2024-08-22T13:32:03'),
+        author: 
+        {
+          id: 3,
+          firstname: "Mathilde",
+          lastname: "kalel",
+          gender: "M",
+          mails: ["mail8@aelion.com"],
+          phone: "0605040311",
+          occupation: "Assistante d'acceuil",
+          company: {
+              id: 1,
+              name: "AELION"
+              },
+          poe: {
+              id: 1,
+              beginAt: new Date("2024/05/05"),
+              endAt: new Date("2024/07/05"),
+              name: "POEI DEV MOB",
+              type: "POEI"
           }
       },
         category: PostCategory.news,
