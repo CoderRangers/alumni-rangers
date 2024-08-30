e8c56008c7f74361b15c3e7f14d6c7e06d609d2d
Antoine RAULT <30930719+arault@users.noreply.github.com>
2024-08-30 15:01:00 +0200

    Add an online status bullet to avatar

--- alumni-mob/src/app/shared/components/avatar-with-status/avatar-with-status.component.ts
+++ alumni-mob/src/app/shared/components/avatar-with-status/avatar-with-status.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { InternType } from 'src/app/core/types/intern/intern-type';
 
 @Component({
   selector: 'app-avatar-with-status',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avatar-with-status.component.scss'],
 })
 export class AvatarWithStatusComponent  implements OnInit {
+
+  @Input()
+  public intern!: InternType
+
+  public isOnline: boolean = false
 
   constructor() { }
 
