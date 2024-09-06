3ff58834c6e54d426c3cf7dfbf31fcf9c106e454
jean <jean.soulie@poe.aelion.fr>
2024-09-06 14:29:55 +0200

    test login without JWT

--- alumni-mob/src/app/login/components/signin.component.ts
+++ alumni-mob/src/app/login/components/signin.component.ts
@@ -14,101 +14,156 @@ import { WsChatService } from 'src/app/core/services/ws-chat.service';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss'],
 })
-export class SigninComponent  implements OnInit {
-
-  public form: FormGroup = new FormGroup({})
+export class SigninComponent implements OnInit {
+  public form: FormGroup = new FormGroup({});
 
   constructor(
     private _formBuilder: FormBuilder,
     private _service: LoginService,
     private _toastController: ToastController,
     private _router: Router,
-    private _storage: StorageService, 
+    private _storage: StorageService,
     private _wsService: WsChatService
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.form = this._formBuilder.group({
       login: [
         '', // Default value for the control
-        [
-          Validators.required,
-        ]
+        [Validators.required],
       ],
-      password: [
-        '',
-        [
-          Validators.required,
-        ]
-      ]
-    })
+      password: ['', [Validators.required]],
+    });
   }
+
+  //   onSubmit(): void {
+  //     // console.log(`Bout to send ${JSON.stringify(this.form.value)}`)
+  //     this._service.login(this.form.value)
+  //       .pipe(
+  //         take(1)
+  //       )
+  //       .subscribe({
+  //         next: async(response: string) => {
+  //           if (response.status == 200) {
+  //             this._storage.store('auth', response.body.token)
+  //             this._router.navigate(['tabs','tab1'])
+  //               .then(() => {
+  //                 console.log('Routing complete')
+  //                 this._wsService.connect()
+  //                 this._wsService.receiveIdentity()
+  //                 .subscribe((identity: any) => {
+  //                     console.log(`got ${identity.socketId} from Socket Server`)
+  //                     const userId: string = ((response.body.token) as string).split('.')[0]
+  //                     const message: any = {
+  //                       socketId: identity.socketId,
+  //                       id: userId
+  //                     }
+  //                     this._wsService.sendIdentity(message)
+  //                   }
+  //                 )
+  //               })
+  //             this.form.reset()
+  //           }
+  //           else {
+  //             const toast = await this._toastController.create({
+  //               message: response.body.message,
+  //               duration: 2000,
+  //               position: 'middle',
+  //               buttons: [
+  //                 {
+  //                   text: 'Réessayer',
+  //                 }
+  //               ]
+  //             })
+  //             /* // equivalent du await au dessus
+  //             let legacyToastPromise
+  //             this._toastController.create({
+  //               message: response.body.message,
+  //               duration: 2000,
+  //               position: 'middle',
+  //               buttons: [
+  //                 {
+  //                   text: 'Réessayer',
+  //                 }
+  //               ]
+  //             }).then((toast) => legacyToastPromise = toast)
+  //             */
+
+  //             await toast.present()
+  //             toast.onWillDismiss()
+  //               .then(() => this.form.reset())
+  //           }
+  //         },
+  //         error: (error: any) => {
+  //           console.log(`ko, je dois afficher un toast ${JSON.stringify(error)}`)
+  //         }
+  //       })
+  //   }
+
+  // }
+
+  //   onSubmit(): void {
+  //     // Envoie les informations d'identification à la gateway
+  //     this._service
+  //       .login(this.form.value)
+  //       .pipe(take(1))
+  //       .subscribe({
+  //         next: async (response: any) => {
+  //           // Stocke le token et navigue vers le tableau principal
+  //           this._storage.store('auth', response.body.token);
+  //           await this._router.navigate(['tabs', 'tab1']);
+
+  //           console.log('Routing complete');
+  //           this._wsService.connect();
+  //           this._wsService.receiveIdentity().subscribe((identity: any) => {
+  //             console.log(`got ${identity.socketId} from Socket Server`);
+  //             const userId: string = response.body.token.split('.')[0];
+  //             const message: any = {
+  //               socketId: identity.socketId,
+  //               id: userId,
+  //             };
+  //             this._wsService.sendIdentity(message);
+  //           });
+
+  //           // Réinitialise le formulaire après la connexion
+  //           this.form.reset();
+  //         },
+  //         error: (error: any) => {
+  //           // Affiche un toast en cas d'erreur
+  //           console.log(`Erreur lors de la connexion: ${JSON.stringify(error)}`);
+  //           const toast = this._toastController.create({
+  //             message: "Une erreur s'est produite lors de la connexion.",
+  //             duration: 2000,
+  //             position: 'middle',
+  //             buttons: [{ text: 'Réessayer' }],
+  //           });
+  //           toast.then((t) => t.present());
+  //         },
+  //       });
+  //   }
+  // }
 
   onSubmit(): void {
-    // console.log(`Bout to send ${JSON.stringify(this.form.value)}`)
-    this._service.doLogin(this.form.value)
-      .pipe(
-        take(1)
-      )
+    // Envoie les informations d'identification à la gateway
+    const value = { email : this.form.value.login, pwd :this.form.value.password };
+    this._service
+      .login(value)
+      .pipe(take(1))
       .subscribe({
-        next: async(response: HttpResponse<any>) => {
-          if (response.status == 200) {
-            this._storage.store('auth', response.body.token)
-            this._router.navigate(['tabs','tab1'])
-              .then(() => {
-                console.log('Routing complete')
-                this._wsService.connect()
-                this._wsService.receiveIdentity()
-                .subscribe((identity: any) => {
-                    console.log(`got ${identity.socketId} from Socket Server`)
-                    const userId: string = ((response.body.token) as string).split('.')[0]
-                    const message: any = {
-                      socketId: identity.socketId,
-                      id: userId
-                    }
-                    this._wsService.sendIdentity(message)
-                  }
-                )
-              })
-            this.form.reset()
-          }
-          else {
-            const toast = await this._toastController.create({
-              message: response.body.message,
-              duration: 2000,
-              position: 'middle',
-              buttons: [
-                {
-                  text: 'Réessayer',
-                }
-              ]
-            })
-            /* // equivalent du await au dessus
-            let legacyToastPromise
-            this._toastController.create({
-              message: response.body.message,
-              duration: 2000,
-              position: 'middle',
-              buttons: [
-                {
-                  text: 'Réessayer',
-                }
-              ]
-            }).then((toast) => legacyToastPromise = toast)
-            */
-
-
-            await toast.present()
-            toast.onWillDismiss()
-              .then(() => this.form.reset())
-          }
+        next: async (response: any) => {
+          console.log(response, value);
         },
         error: (error: any) => {
-          console.log(`ko, je dois afficher un toast ${JSON.stringify(error)}`)
-        }
-      })
+          // Affiche un toast en cas d'erreur
+          console.log(`Erreur lors de la connexion: ${JSON.stringify(error)}`);
+          const toast = this._toastController.create({
+            message: "Une erreur s'est produite lors de la connexion.",
+            duration: 2000,
+            position: 'middle',
+            buttons: [{ text: 'Réessayer' }],
+          });
+          toast.then((t) => t.present());
+        },
+      });
   }
-
-
 }
--- alumni-mob/src/app/login/services/login.service.ts
+++ alumni-mob/src/app/login/services/login.service.ts
@@ -1,13 +1,15 @@
-import { HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/account'; 
+  constructor(
+    private _httpClient: HttpClient
+  ) {}
 
   doLogin(credentials: any): Observable<HttpResponse<any>> {
     if (credentials.login === 'anthoine' && credentials.password === 'admin') {
@@ -15,27 +17,76 @@ export class LoginService {
       return of(
         new HttpResponse<any>({
           status: 200,
-          body: { token: '66cee5f6ad0c7c5a1b396afb.b.c' }
+          body: { token: '66cee5f6ad0c7c5a1b396afb.b.c' },
         })
-      )
-    }
-    else {
+      );
+    } else {
       if (credentials.login === 'cyril' && credentials.password === 'admin') {
         // Ici retourner un 200 ok
         return of(
           new HttpResponse<any>({
             status: 200,
-            body: { token: '66cee252ad0c7c5a1b396af3.b.c' }
+            body: { token: '66cee252ad0c7c5a1b396af3.b.c' },
           })
-        )
+        );
       }
       // Return response 403 Forbiden
       return of(
         new HttpResponse<any>({
           status: 403,
-          body: { message: `Echec de l'identification` }
+          body: { message: `Echec de l'identification` },
         })
-      )
+      );
     }
   }
+
+  // login(credentials: any):Observable<string> {
+  //   // Envoie les informations d'identification à la gateway
+  //   return this._httpClient.post<string>(
+  //     `${this.apiUrl}/login`,
+  //     credentials
+  //   );
+  // }
+
+  login(credentials: any):Observable<string> {
+    console.log(credentials)
+    // Envoie les informations d'identification à la gateway
+    return this._httpClient.post<string>(
+      `${this.apiUrl}`,
+      credentials
+    );
+  }
 }
+
+// import { Injectable } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthService {
+//   private apiUrl = 'https://votre-api.com'; // Remplacez par votre URL d'API
+//   private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+
+//   constructor(private http: HttpClient, private router: Router) {}
+
+//   logout() {
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('userId');
+//     // this.isLoggedInSubject.next(false);
+//     this.router.navigate(['/login']);
+//   }
+
+//   isLoggedIn() {
+//     // return this.isLoggedInSubject.asObservable();
+//   }
+
+//   getToken() {
+//     return localStorage.getItem('token');
+//   }
+
+//   getUserId() {
+//     return localStorage.getItem('userId');
+//   }
+// }
