8e2a7a7dabb9fb72bed223a7e3d53789855c9f44
Julien <julien.maure@poe.aelion.fr>
2024-09-06 13:42:58 +0200

    return token debuged

--- gateway/src/auth/auth.service.ts
+++ gateway/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { take } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 import { AccountService } from 'src/account/account.service';
-import { AccountType } from 'src/account/model/account-type';
+// import { AccountType } from 'src/account/model/account-type';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -12,20 +12,13 @@ export class AuthService {
   ) {}
 
   async signIn(email: string, pwd: string): Promise<any> {
-    this._accountService
-      .login(email, pwd)
-      .pipe(take(1))
-      .subscribe({
-        next: async (account: AccountType | null) => {
-          if (account === null) {
-            return null;
-          } else {
-            const payload = { sub: account.id, username: account.email };
-            return {
-              access_token: await this.jwtService.signAsync(payload),
-            };
-          }
-        },
-      });
+    const account = await lastValueFrom(this._accountService.login(email, pwd));
+    if (account) {
+      const payload = { sub: account.id, username: account.email };
+      const access_token = await this.jwtService.signAsync(payload);
+      return {
+        access_token,
+      };
+    }
   }
 }
