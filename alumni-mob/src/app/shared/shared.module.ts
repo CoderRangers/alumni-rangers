b27497ac0aaeeed0bbfbcf9dff78361648c8b169
Remi <remi.salvan@poe.aelion.fr>
2024-09-09 16:03:29 +0200

    add avatarWithStatusComponent import to sharedModule.ts

--- alumni-mob/src/app/shared/shared.module.ts
+++ alumni-mob/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { InternHeaderComponent } from './components/intern-header/intern-header.component';
-import { AvatarWithStatusComponent } from './components/avatar-with-status/avatar-with-status.component';
+
 import { ChatComponent } from './components/chat/chat.component';
 import { AvatarWithStatusComponent } from './components/avatar-with-status/avatar-with-status.component';
 
