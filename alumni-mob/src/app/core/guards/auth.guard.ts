c21688cf2a0d385b76ad2d213b9721727f64cbd4
Antoine RAULT <30930719+arault@users.noreply.github.com>
2024-09-09 16:17:02 +0200

    Frontend: implement JWT checking in AuthGuard

--- alumni-mob/src/app/core/guards/auth.guard.ts
+++ alumni-mob/src/app/core/guards/auth.guard.ts
@@ -1,23 +1,49 @@
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { StorageService } from '../services/storage.service';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  private readonly _gatewayBaseURI = 'http://localhost:3000';
 
-  constructor (
+  constructor(
     private _storageService: StorageService,
-    private _router: Router
-  ) { }
+    private _router: Router,
+    private _httpClient: HttpClient
+  ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    if (this._storageService.retrieve('auth'))
-      // TODO: check that the value corresponding to 'auth' in localStorage is a valid JWT
-      return true
-    
-    this._router.navigate(['/','login'])
-    return false
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    const token = this._storageService.retrieve('auth');
+    let isTokenValid: boolean = false;
+    if (token) {
+      this._httpClient
+        .get<boolean>(`${this._gatewayBaseURI}/auth/token-check`, token)
+        .pipe(take(1))
+        .subscribe({
+          next: (response) => {
+            response ? (isTokenValid = true) : (isTokenValid = false);
+          },
+        });
+    }
+    if (isTokenValid) {
+      return true;
+    } else {
+      this._router.navigate(['/', 'login']);
+      return false;
+    }
   }
 }
