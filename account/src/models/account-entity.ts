7b839bae2e39ab7bb7b4421f04e1a1e791feb9c2
Remi <remi.salvan@poe.aelion.fr>
2024-09-10 11:51:41 +0200

    adding internId column in accountEntity and accountType in account microservice

--- account/src/models/account-entity.ts
+++ account/src/models/account-entity.ts
@@ -27,4 +27,6 @@ export class AccountEntity {
 
   @Column()
   isActivated: boolean;
+  @Column()
+  internID: string;
 }
--- account/src/models/account.type.ts
+++ account/src/models/account.type.ts
@@ -4,4 +4,5 @@ export type AccountType = {
   password: string;
   role: 'intern' | 'admin' | 'superadmin';
   isActivated: boolean;
+  internId: string;
 };
