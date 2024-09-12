5cb8175831047bda9e5cccbafc35b5a3de654735
Remi <remi.salvan@poe.aelion.fr>
2024-09-12 14:25:19 +0200

    put appController test into test directory and create testing file for appService

---
+++ post/test/app.controller.spec.ts
@@ -0,0 +1,54 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+import { PostCategory, PostType } from '../src/models/post-type';
+
+const mockPost: Array<PostType> = [
+  {
+    id: '1',
+    title: 'First Post',
+    content: 'This is the content of the first post.',
+    media: 'http://example.com/media1.jpg',
+    postedAt: new Date('2023-10-01T10:00:00Z'),
+    author: {
+      id: 'author1',
+      firstname: 'John Doe',
+      lastname: 'jeanjean',
+      emails: ['john.doe@example.com'],
+      gender: 'indefined',
+      phone: 'nonantetrois',
+      occupation: 'branleur professionel',
+      poe: {
+        id: 1,
+        beginAt: new Date('2023-10-01'),
+        endAt: new Date('2023-10-01'),
+        name: 'dev pasweb',
+        type: 'feu',
+      },
+    },
+    likes: [],
+    comments: [],
+    category: PostCategory.news,
+  },
+];
+
+describe('AppController', () => {
+  let appController: AppController;
+  const appService: AppService = new AppService(null, null);
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [{ provide: AppService, useValue: appService }],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    //appService = app.get<AppService>(AppService);
+  });
+
+  it('findAll()', async () => {
+    jest.spyOn(appService, 'getAllPosts').mockResolvedValue(mockPost);
+    const result = await appController.findAll();
+    expect(result).toBe(mockPost);
+  });
+});
---
+++ post/test/app.service.spec.ts
@@ -0,0 +1,29 @@
+/*const mockPost: Array<PostType> = [
+  {
+    id: '1',
+    title: 'First Post',
+    content: 'This is the content of the first post.',
+    media: 'http://example.com/media1.jpg',
+    postedAt: new Date('2023-10-01T10:00:00Z'),
+    author: {
+      id: 'author1',
+      firstname: 'John Doe',
+      lastname: 'jeanjean',
+      emails: ['john.doe@example.com'],
+      gender: 'indefined',
+      phone: 'nonantetrois',
+      occupation: 'branleur professionel',
+      poe: {
+        id: 1,
+        beginAt: new Date('2023-10-01'),
+        endAt: new Date('2023-10-01'),
+        name: 'dev pasweb',
+        type: 'feu',
+      },
+    },
+    likes: [],
+    comments: [],
+    category: PostCategory.news,
+  },
+];
+describe("")*/
