7c8e4964a427fb194fdc2441c1a427a06c7cb315
jean <jean.soulie@poe.aelion.fr>
2024-09-19 15:22:32 +0200

    implementation of remove, update and insert methods

--- company-feedback/src/app.controller.ts
+++ company-feedback/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CompanyFeedbackType } from './models/company-feedback.type';
+import { CompanyFeedbackEntity } from './models/company-feedback.entity';
 
 @Controller()
 export class AppController {
@@ -19,8 +20,23 @@ export class AppController {
     return oneFeedback;
   }
 
-  // @MessagePattern({ cmd: 'removeCompanyFeedback' })
-  // async removeCompanyFeeedback(id: string) {
-  //   return this.appService.removeFeedback(id);
-  // }
+  @MessagePattern({ cmd: 'removeCompanyFeedback' })
+  async deleteCompanyFeeedback(id: string) {
+    return this.appService.removeFeedback(id);
+  }
+
+  @MessagePattern({ cmd: 'updateCompanyFeedback' })
+  async changeCompanyFeedback(
+    idFeedback: string,
+    updatedData: Partial<CompanyFeedbackEntity>,
+  ): Promise<CompanyFeedbackEntity> {
+    return this.appService.updateFeedback(idFeedback, updatedData);
+  }
+
+  @MessagePattern({ cmd: 'createCompanyFeedback' })
+  newCompanyFeedback(
+    feedbackData: CompanyFeedbackType,
+  ): Promise<CompanyFeedbackEntity> {
+    return this.appService.insertFeedback(feedbackData);
+  }
 }
--- company-feedback/src/app.service.ts
+++ company-feedback/src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CompanyFeedbackType } from './models/company-feedback.type';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyFeedbackEntity } from './models/company-feedback.entity';
@@ -10,6 +10,7 @@ export class AppService {
     @InjectRepository(CompanyFeedbackEntity) //it works without add companyEntity.... weird !
     private _repository: Repository<CompanyFeedbackEntity>,
   ) {}
+
   async getAllFeedback(): Promise<CompanyFeedbackType[]> {
     const feedbacks = await this._repository.find({
       relations: ['company'],
@@ -53,7 +54,48 @@ export class AppService {
     };
   }
 
-  // removeFeedback(idFeedback: string): Promise<CompanyFeedbackEntity> {
-  //   return this._repository.delete({ id: idFeedback });
-  // }
+  removeFeedback(idFeedback: string): Promise<CompanyFeedbackEntity> {
+    const feedback = this._repository.findOne({
+      where: { id: idFeedback },
+    });
+
+    this._repository.delete({ id: idFeedback });
+    // Feedback contains either a promise of the deleted record, or a promise of null
+    return feedback;
+  }
+
+  updateFeedback(
+    idFeedback: string,
+    updatedData: Partial<CompanyFeedbackEntity>,
+  ): Promise<CompanyFeedbackEntity> {
+    // Rechercher l'entité à mettre à jour
+    const feedback = this._repository.findOne({
+      where: { id: idFeedback },
+    });
+
+    if (!feedback) {
+      throw new Error(`Feedback with id ${idFeedback} not found`);
+    }
+
+    this._repository.update({ id: idFeedback }, updatedData);
+
+    return { ...feedback, ...updatedData };
+  }
+
+  async insertFeedback(
+    feedbackData: CompanyFeedbackType,
+  ): Promise<CompanyFeedbackEntity> {
+    // Utilisez la méthode insert pour ajouter un nouveau feedback
+    try {
+      const result = await this._repository.insert(feedbackData);
+      // Le résultat contient des informations sur l'insertion
+      // Récupérer l'ID du feedback inséré
+      const insertedId = result.identifiers[0].id;
+      const savedFeedback = await this._repository.findOne(insertedId);
+      // Rechercher l'entité insérée pour retourner l'objet complet
+      return savedFeedback; // Retournez l'entité sauvegardée
+    } catch (error) {
+      Logger.log(error);
+    }
+  }
 }
