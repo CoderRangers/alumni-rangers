0539cea5f30da51269396a3ff8f248228a771687
Julien <julien.maure@poe.aelion.fr>
2024-09-19 16:53:09 +0200

    create CRUD methods on company and feedback services

--- alumni-mob/src/app/core/services/company.service.ts
+++ alumni-mob/src/app/core/services/company.service.ts
@@ -7,24 +7,31 @@ import { CompanyType } from '../types/company-feedback/company-feed.type';
   providedIn: 'root'
 })
 export class CompanyService {
-  private readonly URI = 'http://localhost:3000/company'
+  private readonly URI = 'http://localhost:3000/company';
 
-  private _company!: Observable <Array<CompanyType>>
+  //private _company!: Observable <Array<CompanyType>>
 
   constructor(
     private _httpClient: HttpClient
   ) { }
 
-  findAll(): Observable<Array<CompanyType>> {
+  public findAll(): Observable<Array<CompanyType>> {
     return this._httpClient.get<Array<CompanyType>>(this.URI)
   }
 
   public findOne(id: string): Observable<CompanyType> {
     return this._httpClient.get<CompanyType>(this.URI+'/'+id)
-/*     .pipe(
-      map((intern: any) => {
-        return plainToInstance(InternType, intern)
-      })
-    ) */
+  }
+
+  public create(company: CompanyType): Observable<CompanyType> {
+    return this._httpClient.post<CompanyType>(this.URI, company)
+  }
+
+  public update(id: string, data: CompanyType): Observable<CompanyType> {
+    return this._httpClient.put<CompanyType>(this.URI+'/'+id, data);
+  }
+
+  public delete(id: string): Observable<CompanyType> {
+    return this._httpClient.delete<CompanyType>(this.URI+'/'+id);
   }
 }
--- alumni-mob/src/app/core/services/feedback.service.ts
+++ alumni-mob/src/app/core/services/feedback.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CompanyFeedbackType } from '../types/company-feedback/company-feedback.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
+  private readonly URI = 'http://localhost:3000/feedback';
 
-  constructor() { }
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  public findAll(): Observable<Array<CompanyFeedbackType>> {
+    return this._httpClient.get<Array<CompanyFeedbackType>>(this.URI)
+  }
+
+  public findOne(id: string): Observable<CompanyFeedbackType> {
+    return this._httpClient.get<CompanyFeedbackType>(this.URI+'/'+id)
+  }
+
+  public create(feedback: CompanyFeedbackType): Observable<CompanyFeedbackType> {
+    return this._httpClient.post<CompanyFeedbackType>(this.URI, feedback)
+  }
+
+  public update(id: string, data: CompanyFeedbackType): Observable<CompanyFeedbackType> {
+    return this._httpClient.put<CompanyFeedbackType>(this.URI+'/'+id, data);
+  }
+
+  public delete(id: string): Observable<CompanyFeedbackType> {
+    return this._httpClient.delete<CompanyFeedbackType>(this.URI+'/'+id);
+  }
 }
