1d2248f755eb3d46bfe5fe3ff738b336a9fdb6ee
Remi <remi.salvan@poe.aelion.fr>
2024-09-19 13:45:23 +0200

    update handling of null result when looking up 1+ feedback/company

--- company-feedback/src/app.service.ts
+++ company-feedback/src/app.service.ts
@@ -7,33 +7,53 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AppService {
   constructor(
-    @InjectRepository(CompanyFeedbackEntity)
+    @InjectRepository(CompanyFeedbackEntity) //it works without add companyEntity.... weird !
     private _repository: Repository<CompanyFeedbackEntity>,
   ) {}
   async getAllFeedback(): Promise<CompanyFeedbackType[]> {
     const feedbacks = await this._repository.find({
       relations: ['company'],
     });
-
+    feedbacks.forEach((feedback) => {
+      if (!feedback || !feedback.company) {
+        throw new Error('Feedback not found or company relation is missing');
+      }
+    });
     return feedbacks.map((feedback) => ({
       ...feedback,
-      company: feedback.company
-        ? {
-            id: feedback.company.id,
-            name: feedback.company.name,
-            type: feedback.company.type,
-            medianRating: feedback.company.medianRating,
-            logo: feedback.company.logo,
-          }
-        : null,
+      company: {
+        id: feedback.company.id,
+        name: feedback.company.name,
+        type: feedback.company.type,
+        medianRating: feedback.company.medianRating,
+        logo: feedback.company.logo,
+      },
     }));
   }
 
-  getOneFeedback(idFeedback: string): Promise<CompanyFeedbackEntity> {
-    return this._repository.findOneBy({ id: idFeedback });
+  async getOneFeedback(idFeedback: string): Promise<CompanyFeedbackType> {
+    const feedback = await this._repository.findOne({
+      where: { id: idFeedback },
+      relations: ['company'],
+    });
+
+    if (!feedback || !feedback.company) {
+      throw new Error('Feedback not found or company relation is missing');
+    }
+
+    return {
+      ...feedback,
+      company: {
+        id: feedback.company.id,
+        name: feedback.company.name,
+        type: feedback.company.type,
+        medianRating: feedback.company.medianRating,
+        logo: feedback.company.logo,
+      },
+    };
   }
 
-  removeFeedback(idFeedback: string): Promise<CompanyFeedbackEntity> {
-    return this._repository.delete({ id: idFeedback });
-  }
+  // removeFeedback(idFeedback: string): Promise<CompanyFeedbackEntity> {
+  //   return this._repository.delete({ id: idFeedback });
+  // }
 }
