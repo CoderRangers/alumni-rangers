79d0b1e9125427ba69f2477aca37b256b31c8463
Remi <remi.salvan@poe.aelion.fr>
2024-09-20 13:44:41 +0200

    updating companyModule

--- company-feedback/src/company/company.module.ts
+++ company-feedback/src/company/company.module.ts
@@ -3,10 +3,12 @@ import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyEntity } from 'src/models/company.entity';
+import { AppService } from 'src/app.service';
+import { CompanyFeedbackEntity } from 'src/models/company-feedback.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CompanyEntity])],
+  imports: [TypeOrmModule.forFeature([CompanyEntity, CompanyFeedbackEntity])],
   controllers: [CompanyController],
-  providers: [CompanyService],
+  providers: [CompanyService, AppService],
 })
 export class CompanyModule {}
