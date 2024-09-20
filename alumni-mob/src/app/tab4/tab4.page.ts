c938a2c28a0b370ce06740972230401961c2c686
jean <jean.soulie@poe.aelion.fr>
2024-09-20 13:56:39 +0200

    Front-end: tab4 + button now displays firststep of feedback form

--- alumni-mob/src/app/tab4/tab4.page.ts
+++ alumni-mob/src/app/tab4/tab4.page.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CompanyCategory, CompanyType } from '../core/types/company-feedback/company-feed.type';
+import {
+  CompanyCategory,
+  CompanyType,
+} from '../core/types/company-feedback/company-feed.type';
 import { CompanyRating } from '../core/types/company-feedback/company-rating.type';
 import { IonInput } from '@ionic/angular';
 import { CompanyService } from '../core/services/company.service';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { FeedbackFormStep1Component } from './components/feedback-form-step1/feedback-form-step1.component';
 
 @Component({
   selector: 'app-tab4',
@@ -12,37 +17,50 @@ import { Router } from '@angular/router';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
-  public companys!: Array<CompanyType>
-  public filteredComp!: Array<CompanyType>
+  public companys!: Array<CompanyType>;
+  public filteredComp!: Array<CompanyType>;
   public inputModel = '';
 
-  constructor(private _companyService: CompanyService, private _router: Router) { }
+  constructor(
+    private _companyService: CompanyService,
+    private _router: Router,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
-    this._companyService.findAll()
-    .pipe(take(1))
-    .subscribe({
-      next: (response: any) => {
-        this.companys = response;
-        this.filteredComp = this.companys;
-      },
-    });  
+    this._companyService
+      .findAll()
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          this.companys = response;
+          this.filteredComp = this.companys;
+        },
+      });
   }
 
   @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
   onInput(ev: any) {
     const value = ev.target!.value;
-    this.filteredComp = this.companys.filter((comp) => comp.name.toLowerCase().includes(value.toLowerCase()));
+    this.filteredComp = this.companys.filter((comp) =>
+      comp.name.toLowerCase().includes(value.toLowerCase())
+    );
   }
 
   selectCompany(companyId: string) {
     console.log(`click ${companyId}`);
-    this._router.navigate(['tabs','tab4','company-feedbacks', companyId]);
+    this._router.navigate(['tabs', 'tab4', 'company-feedbacks', companyId]);
   }
 
-  addFeedback() {
-    console.log('add feedback');
+  async dismiss() {
+    await this.modalController.dismiss();
+  }
+
+  async addFeedback() {
+    const modal = await this.modalController.create({
+      component: FeedbackFormStep1Component,
+    });
+    modal.present();
   }
 
   private populate(): CompanyType[] {
@@ -52,14 +70,14 @@ export class Tab4Page implements OnInit {
         name: 'Google',
         type: CompanyCategory.largeGroup,
         medianRating: CompanyRating.slightlyPositive,
-        logo: '/assets/icon/logo-de-vecteur-google-chrome-passent-le-moteur-recherche-au-bichromate-potasse-du-pour-les-marques-célèbres-internationales-189260093.webp'
+        logo: '/assets/icon/logo-de-vecteur-google-chrome-passent-le-moteur-recherche-au-bichromate-potasse-du-pour-les-marques-célèbres-internationales-189260093.webp',
       },
       {
         id: '2',
         name: 'Apple',
         type: CompanyCategory.largeGroup,
         medianRating: CompanyRating.veryNegative,
-        logo: '/assets/Le-logo-Apple-500x281.jpg'
+        logo: '/assets/Le-logo-Apple-500x281.jpg',
       },
       // ... Ajoutez ici les 2 autres éléments
       {
@@ -67,15 +85,15 @@ export class Tab4Page implements OnInit {
         name: 'Tesla',
         type: CompanyCategory.smallAndMediumCompany,
         medianRating: CompanyRating.neutral,
-        logo: '/assets/icon/Logo-Tesla-noir.jpg'
+        logo: '/assets/icon/Logo-Tesla-noir.jpg',
       },
       {
         id: '4',
         name: 'Starbucks',
         type: CompanyCategory.startUp,
         medianRating: CompanyRating.veryPositive,
-        logo: "/assets/icon/starbucks logo current.webp"
-      }
+        logo: '/assets/icon/starbucks logo current.webp',
+      },
     ];
   }
 }
