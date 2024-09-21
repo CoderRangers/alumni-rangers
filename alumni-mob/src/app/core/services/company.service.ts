10b05e4b04391891f86bce3b90c0f9846dac8ab1
Remi <remi.salvan@poe.aelion.fr>
2024-09-21 17:33:11 +0200

    adding Check if company already exists before creation

--- alumni-mob/src/app/core/services/company.service.ts
+++ alumni-mob/src/app/core/services/company.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError, Observable, tap } from 'rxjs';
+import { catchError, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CompanyType } from '../types/company-feedback/company-feed.type';
 
@@ -23,13 +23,30 @@ export class CompanyService {
     return this._httpClient.get<CompanyType>(this.URI+'/'+id)
   }
 
+/**In create() we use throwError to rise an exception and save the type returned
+ * Because if the company exists already, the type returned is different Observable<null>
+ * throwError preserve the same type Observable<CompanyType>
+ * couldn't fix it adding a pipe operator |
+ */
   public create(company: CompanyType): Observable<CompanyType> {
     console.log('Creating company:', company);
-    return this._httpClient.post<CompanyType>(this.URI, company).pipe(
-      tap(response => console.log('Company created:', response)),
-      catchError(error => {
-        console.error('Error creating company:', error);
-        throw error;
+
+    return this.findAll().pipe(
+      switchMap(companies => {
+        const existingCompany = companies.find(c => c.name.toLowerCase() === company.name.toLowerCase());
+
+        if (existingCompany) {
+          console.error('Company with the same name already exists:', existingCompany);
+          return throwError(() => new Error('Company with the same name already exists'));
+        } else {
+          return this._httpClient.post<CompanyType>(this.URI, company).pipe(
+            tap(response => console.log('Company created:', response)),
+            catchError(error => {
+              console.error('Error creating company:', error);
+              return throwError(() => error);
+            })
+          );
+        }
       })
     );
   }
