d938b25d46a3640101fdd661e22b5e9618d9f9ac
Julien <julien.maure@poe.aelion.fr>
2024-09-23 18:00:32 +0200

    add new header for all steps

--- alumni-mob/src/app/tab4/components/feedback-form-header/feedback-form-header.component.ts
+++ alumni-mob/src/app/tab4/components/feedback-form-header/feedback-form-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -7,6 +7,9 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./feedback-form-header.component.scss'],
 })
 export class FeedbackFormHeaderComponent  implements OnInit {
+  
+  @Input()
+  public isFirstStep: boolean = true;
 
   constructor(private modalCtrl: ModalController) { }
 
