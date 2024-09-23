7edbfcd7ed83a00b25f47bf347393c72de0801ee
Julien <julien.maure@poe.aelion.fr>
2024-09-23 12:43:56 +0200

    add click on name in feedback component who redirect to his profile

--- alumni-mob/src/app/tab4/company-feedbacks/component/feedback/feedback.component.ts
+++ alumni-mob/src/app/tab4/company-feedbacks/component/feedback/feedback.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
+import { StorageService } from 'src/app/core/services/storage.service';
 import { CompanyFeedbackType } from 'src/app/core/types/company-feedback/company-feedback.type';
+import { TokenInfoType } from 'src/app/core/types/login/token-type';
 
 @Component({
   selector: 'app-feedback',
@@ -16,11 +20,16 @@ export class FeedbackComponent  implements OnInit {
 
   @Input()
   public hide: boolean = true;
-
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
+
+  goToProfile() {
+    this._router.navigate(['intern', this.feedback.internId]);
+  }
 
   showAll() {
     this.hide = !this.hide;
